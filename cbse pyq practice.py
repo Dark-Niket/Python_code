606a8a021fb1943448145538afe28b4abc738f55
Dark-Niket <basuniket@gmail.com>
2026-03-20 23:17:20 +0530

    2 questions

--- cbse pyq practice.py
+++ cbse pyq practice.py
@@ -1,13 +1,22 @@
-def remove_element():
-    l = [2,3,4,55,6667,0]
-    m = 0
+# def remove_element():
+#     l = [2,3,4,55,6667,0]
+#     m = 0
 
-    if m in l:
-        l.remove(m)
-        print("Found and removed!!")
-    else:
-        print("Element not found")
+#     if m in l:
+#         l.remove(m)
+#         print("Found and removed!!")
+#     else:
+#         print("Element not found")
 
-    print(l)  # to see updated list
+#     print(l)  
 
-remove_element()
+# remove_element()
+
+
+def add_context():
+    d={"Anita":78,"Diode":89,"Fob":45}
+    for i in d :
+        print (i)
+add_context()
+
+
