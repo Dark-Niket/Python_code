9fd02d7b1f898e716821b49fab2cbe73c26248ce
Dark-Niket <basuniket@gmail.com>
2026-09-05 11:59:30 +0530

    initial commit

--- new7.cc
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float r,a;
-    printf("Enter yout number:");
-    scanf("%f",&r);
-    a=3.14*r*r;
-    printf("%f",a);
-    return 0;
-}
--- new8.cc
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    float r=3.14*3.14;
-    printf("%f",r);
-    return 0;
-}
